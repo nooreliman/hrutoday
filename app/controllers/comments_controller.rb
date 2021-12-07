9b3416f5389d21d3150f295a5808e5c39a0b72ec
Mario Stefan Juhari <msjdtd@gmail.com>
2021-12-07 16:51:22 +0800

    added comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,3 @@
 class CommentsController < ApplicationController
-  def new
-    @post = Post.find(params[:id])
-    @comment = @post.comment.new
-  end
 
-  def create
-    @comment = Comment.new
-  end
 end
