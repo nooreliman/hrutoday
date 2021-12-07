ea854dbec0dd49eaad38c3d0c3ee57eb980c8f11
rachel toh <racheltohxy@gmail.com>
2021-12-07 11:26:10 +0800

    user-model done

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,11 @@ ActiveRecord::Schema.define(version: 2021_12_06_083715) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.integer "age"
+    t.string "photo"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
