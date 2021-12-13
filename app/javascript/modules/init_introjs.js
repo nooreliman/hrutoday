b6fd20626a610067c11af3cc1bb43b1be75654c7
lindacheung <lindacheung94@gmail.com>
2021-12-13 19:14:19 +0800

    added introjs to landing page

--- app/javascript/modules/init_introjs.js
+++ app/javascript/modules/init_introjs.js
@@ -1,26 +1,48 @@
 import introJs from "intro.js";
-// import "intro.js/minified"
+import 'intro.js/introjs.css'
+
 
 const initIntro = () => {
   introJs()
     .setOptions({
+      skipLabel: "Skip",
+
       steps: [
         {
           title: "Welcome",
-          intro: "Hello World! 👋",
+          intro: "Hello there! Welcome to hrutoday!",
         },
         {
           element: document.querySelector(".log-mood"),
-          intro: "This step focuses on an image",
+          intro: "First, you can start by logging your mood for the day.",
+        },
+        {
+          element: document.querySelector(".mood-tracker"),
+          intro: "This is the mood graph to track your moods.",
+        },
+        {
+          element: document.querySelector(".home"),
+          intro: "Returns you to the homepage(here!)",
+        },
+        {
+          element: document.querySelector(".affirm"),
+          intro: "To view our affirmation page and read supportive messages!",
+        },
+        {
+          element: document.querySelector(".forum"),
+          intro: "To view our forums and start posting!",
+        },
+        {
+          element: document.querySelector(".notification"),
+          intro: "Check your notifications here!",
         },
         {
           title: "Farewell!",
-          element: document.querySelector(".card__image"),
-          intro: "And this is our final step!",
+          intro: "And we have come to the end of our tour!",
         },
       ],
     })
     .start();
-};
+  };
 
 export { initIntro };
