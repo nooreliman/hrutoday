7ebf52e075c25a2a5490b4038aeae02dd8a6444a
iman fuad <nooreliman@gmail.com>
2021-12-13 16:23:29 +0800

    dynamic bgc for posts index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_102909) do
+ActiveRecord::Schema.define(version: 2021_12_13_081224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2021_12_12_102909) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "color"
   end
 
   create_table "notifications", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,7 @@ puts "Creating moods"
 mood_happy = Mood.new(
   name: "Happy",
   image: "happy.png"
+  color: "#bfd8e5"
 )
 
 mood_happy.save!
@@ -44,6 +45,7 @@ mood_happy.save!
 mood_sad = Mood.new(
   name: "Sad",
   image: "sad.png"
+  color: "#cbc6d6"
 )
 
 mood_sad.save!
@@ -51,6 +53,7 @@ mood_sad.save!
 mood_angry = Mood.new(
   name: "Angry",
   image: "angry.png"
+  color: "#dea6a6"
 )
 
 mood_angry.save!
@@ -58,6 +61,7 @@ mood_angry.save!
 mood_scared = Mood.new(
   name: "Scared",
   image: "scared.png"
+  color: "#e7ebbc"
 )
 
 mood_scared.save!
@@ -65,6 +69,7 @@ mood_scared.save!
 mood_love = Mood.new(
   name: "Love",
   image: "love.png"
+  color: "#e1bddb"
 )
 
 mood_love.save!
@@ -72,6 +77,7 @@ mood_love.save!
 mood_confused = Mood.new(
   name: "Confused",
   image: "confused.png"
+  color: "#b5dad1"
 )
 
 mood_confused.save!
