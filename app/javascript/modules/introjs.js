f9882b8070f4844e35cc58952e557d89ed40bdd3
lindacheung <lindacheung94@gmail.com>
2021-12-14 09:04:28 +0800

    added in extra condition

--- app/javascript/modules/introjs.js
+++ app/javascript/modules/introjs.js
@@ -5,64 +5,60 @@ var intro = introJs();
 
 const initIntro = () => {
   window.addEventListener("load", function () {
-  var hasRunIntro = localStorage.getItem("hasRunIntro");
-    if (hasRunIntro !== "1") {
-    intro
-      .setOptions({
-        skipLabel: "Skip",
+    var hasRunIntro = localStorage.getItem("hasRunIntro");
+    if (hasRunIntro !== "1" && window.location.pathname === "/") {
+      intro
+        .setOptions({
+          skipLabel: "Skip",
 
-        steps: [
-          {
-            title: "Welcome",
-            intro: "Hello there! Welcome to hrutoday!",
-          },
-          {
-            element: document.querySelector(".log-mood"),
-            intro: "First, you can start by logging your mood for the day.",
-          },
-          {
-            element: document.querySelector(".mood-tracker"),
-            intro: "This is the mood graph to track your moods.",
-          },
-          {
-            element: document.querySelector(".home"),
-            intro: "Returns you to the homepage(here!)",
-          },
-          {
-            element: document.querySelector(".affirm"),
-            intro: "To view our affirmation page and read supportive messages!",
-          },
-          {
-            element: document.querySelector(".forum"),
-            intro: "To view our forums and start posting!",
-          },
-          {
-            element: document.querySelector(".notification"),
-            intro: "Check your notifications here!",
-          },
-          {
-            title: "Farewell!",
-            intro: "And we have come to the end of our tour!",
-          },
-        ],
-      })
-      .start();
+          steps: [
+            {
+              title: "Welcome",
+              intro: "Hello there! Welcome to hrutoday!",
+            },
+            {
+              element: document.querySelector(".log-mood"),
+              intro: "First, you can start by logging your mood for the day.",
+            },
+            {
+              element: document.querySelector(".mood-tracker"),
+              intro: "This is the mood graph to track your moods.",
+            },
+            {
+              element: document.querySelector(".home"),
+              intro: "Returns you to the homepage(here!)",
+            },
+            {
+              element: document.querySelector(".affirm"),
+              intro:
+                "To view our affirmation page and read supportive messages!",
+            },
+            {
+              element: document.querySelector(".forum"),
+              intro: "To view our forums and start posting!",
+            },
+            {
+              element: document.querySelector(".notification"),
+              intro: "Check your notifications here!",
+            },
+            {
+              title: "Farewell!",
+              intro: "And we have come to the end of our tour!",
+            },
+          ],
+        })
+        .start();
 
       intro.oncomplete(function () {
         localStorage.setItem("hasRunIntro", "1");
       });
       intro.onexit(function () {
-        localStorage.setItem("hasRunIntro", "1")
-
+        localStorage.setItem("hasRunIntro", "1");
       });
-
     } else {
-
       return;
     }
-  },
-)};
+  });
+};
 
 export { initIntro };
-
-
