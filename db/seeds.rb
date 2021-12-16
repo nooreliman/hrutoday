c592a6b085bef043c729e3a74cfb2f6a5e6d6de0
Mario Stefan Juhari <msjdtd@gmail.com>
2021-12-16 17:33:24 +0800

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -178,226 +178,128 @@ puts "6 Mood Forums created"
 
 puts "Creating posts"
 
-post_happy = Post.new(
-  title: "I am happy",
-  content: "Blehhhhh",
-  status: "true",
-)
-
-post_happy.forum = forum_happy
-post_happy.user = user
-post_happy.save!
-
-post_sad = Post.new(
-  title: "I am sad",
-  content: "Blehhhhh",
-  status: "true",
-)
-
-post_sad.forum = forum_sad
-post_sad.user = user
-post_sad.save!
-
 post_angry = Post.new(
-  title: "I am angry",
-  content: "Blehhhhh",
+  title: "driving to slow",
+  content: "Every one has to have some complaint in regards to this subject. One day when I was leaving my house heading to town I see a small Toyota truck coming out of the drive way of a neighbor’s yard. He is 5 drive ways down. He waits and then pulls out just in front of me, causing me to break so I don’t hit the jackass. Then he proceeds to drive at 15 miles an hour talking to the guy with him, point out the window at this and that. There was no one behind me, he could have waited till I passed, but no. For half a mile I had to follow the jackass, between oncoming traffic, sharp turns, his swerving all over and speed bumps I could not pass him. Man was I happy when he turned off and I could go 45 , get to town before I shriveled and died.",
   status: "true",
 )
 
 post_angry.forum = forum_angry
-post_angry.user = user
+post_angry.user = john
 post_angry.save!
 
+post_angry1 = Post.new(
+  title: "green light hold up",
+  content: "Arrived at a green light with a long line of cars. The lane wasn’t moving. Waited. Still not moving. If the lane was moving, then it was really slow. Then the light turned red. wtf? What the fuck was the hold up? It would be less infuriating if I knew.",
+  status: "true",
+)
+
+post_angry1.forum = forum_angry
+post_angry1.user = paul
+post_angry1.save!
+
+post_angry2 = Post.new(
+  title: "How to stop being angry?",
+  content: "I look calm on the surface but boiling right below it. I don't need a reason to go from cold to hot. I feel like people are to blame for me being unhappy. My ex girlfriend who constantly pushed events even though she knew I had to focus on my study first and then think how we will meet. My father who sold flat that should have been mine not long before my grandmother was ill with cancer. My friend who never understood where I was coming from. My other mate told me I am week and that was not the case. I am working hard to finish study , get a good job and finally start being proud of my accomplishment. That's all I want. It is hard to be angry but it comes out of me .I pray to God. I hope I change.",
+  status: "true",
+)
+
+post_angry2.forum = forum_angry
+post_angry2.user = george
+post_angry2.save!
+
+post_angry3 = Post.new(
+  title: "That doesn't make sense",
+  content: "If I don't like it then I don't like it Black and white, You can't force people's to do something that they don't like it from speaking your voice and action, vice versa, etc. If I don't like dogs then you can not force on me and preach being open and tolerance over people's opinion.
+            That pisses me off having those type of people's for example like vash12349 and Jessi Nigri. I hate those people's with a passionate.",
+  status: "true",
+)
+
+post_angry3.forum = forum_angry
+post_angry3.user = george
+post_angry3.save!
+
+post_angry4 = Post.new(
+  title: "I'm Mad",
+  content: "I been dealing issue and having frustration anger toward small thing to big thing,and i want to release some anger.I get very anger if i don't understand or people's don't understand me and how i am imperfect but i want to very perfect like a god or king,I don't wanna make mistake or people's get the wrong the wrong ideas.
+            I'm angry that i am not perfect and have fault in myself.I have anger over myself on how i make mistake,certain thing repeat itself and how's i don't understand some people's logical, for ex. a person cussed someone out then later on their apologizes for it or trying to be nice. that's when i lose it because in my mind Hello,you just cussed them out and now you wanna be like that. That's what i don't get people's.
+            This is my rant(My belief and Opinion)",
+  status: "true",
+)
+
+post_angry4.forum = forum_angry
+post_angry4.user = george
+post_angry4.save!
+
+
 post_scared = Post.new(
-  title: "I am scared",
-  content: "Blehhhhh",
+  title: "I'm so scared",
+  content: "The week before last the world was pretty great. I was loving my job, walking every day when I got home and planning fun things to do.",
   status: "true",
 )
 
 post_scared.forum = forum_scared
-post_scared.user = user
+post_scared.user = john
 post_scared.save!
 
-post_love = Post.new(
-  title: "I am loved",
-  content: "Blehhhhh",
+post_scared1 = Post.new(
+  title: "Scared/Anxious of the future/current state of the world",
+  content: "Hi everyone this is my first post here. I'm seeing a psychologist but I need some extra help. The current state of the world has me feeling extremely anxious. Im terrified that i won't have a future, i am feeling very hopeless. With the current pandemic, climate change and all the recent news my anxiety is unbearable.",
   status: "true",
 )
 
-post_love.forum = forum_love
-post_love.user = user
-post_love.save!
+post_scared1.forum = forum_scared
+post_scared1.user = paul
+post_scared1.save!
+
+post_scared2 = Post.new(
+  title: "Anyone Else Scared of Seagulls?",
+  content: "Does anyone else find them really annoying? Recently i went to around the shopping area and then bought a sandwich and ate outside. I see those little brown tweety bird and then toss bread at it and i see that bird is eating it in pieces and pieces.",
+  status: "true",
+)
+
+post_scared2.forum = forum_scared
+post_scared2.user = george
+post_scared2.save!
+
+post_scared3 = Post.new(
+  title: "I am very scared that I won't find a job post PhD",
+  content: "I am in possibly the worst place I have been in my life. I regret a lot of things. I regret ever starting PhD, I regret not making more effort in both my PhD and networking aspects. I accept responsibility for my mistakes but it doesn't make my predicament any easier. Any words of advice? Sorry for rambling.",
+  status: "true",
+)
+
+post_scared3.forum = forum_scared
+post_scared3.user = ringo
+post_scared3.save!
+
+post_scared4 = Post.new(
+  title: "Have you ever been scared or nervous when you recieve a reply on your post?",
+  content: "I have...well always, when I post something on forums like Do you think Asians are better than us? I got caught to an argument with a 40 year old there (I think he's a 40 year old cause it says it in his thing...) about people being beautiful and he called me a liar on what I believe and the other time I told this guy to be nice with the kid, but he went and snapped something like replying teddy bears and cookies...",
+  status: "true",
+)
+
+post_scared4.forum = forum_scared
+post_scared4.user = sarah
+post_scared4.save!
 
 post_confused = Post.new(
   title: "I am confused",
-  content: "Blehhhhh",
+  content: "I have a friend that that i like really much and i feel like she gives hints of liking me too, but she says she likes someone else. Am I just overthinking and making myself confused?",
   status: "true",
 )
 
 post_confused.forum = forum_confused
-post_confused.user = user
+post_confused.user = john
 post_confused.save!
 
+post_confused1 = Post.new(
+  title: "I feel like I'm lost in life",
+  content: "Hello everyone! I've never posted on forums but now I'm really desperate and I just wanna tell my story maybe it will help somehow. I'm 26 and lately I feel like my whole world is collapsing. Almost 1 year ago, after the lockdown ended, I decided to quit my job because I was miserable (very low payment, bad working conditions and bad employee treatment ) and I thought that I'll find another one very quickly. I started to apply to different jobs (national and international) but no response from anyone. Months were passing by and still nothing.",
+  status: "true",
+)
+
+post_confused1.forum = forum_confused
+post_confused1.user = paul
+post_confused1.save!
+
 puts "6 Posts created"
-
-puts "Creating comment and reply"
-
-comment_one = Comment.new(
-  content: "How are you?",
-  status: "true",
-)
-
-comment_one.post = post_happy
-comment_one.user = user
-comment_one.save!
-
-reply_one = Comment.new(
-  content: "There's been better days but I'm still coping!",
-  status: "true",
-)
-
-reply_one.post = post_happy
-reply_one.user = user_one
-reply_one.save!
-
-reply_one.reply_to(comment_one)
-
-puts "Comment and reply created"
-
-puts "Creating affirmation messages"
-
-affirm_happy_one = AffirmationMessage.new(
-  message: "Don't know what went down but I'm so proud of you achieving big things!",
-)
-
-affirm_happy_one.user = user
-affirm_happy_one.mood = mood_happy
-affirm_happy_one.save!
-
-affirm_happy_second = AffirmationMessage.new(
-  message: "Good things take time",
-)
-
-affirm_happy_second.user = user
-affirm_happy_second.mood = mood_happy
-affirm_happy_second.save!
-
-affirm_happy_third = AffirmationMessage.new(
-  message: "Love who you",
-)
-
-affirm_happy_third.user = user
-affirm_happy_third.mood = mood_happy
-affirm_happy_third.save!
-
-affirm_happy_four = AffirmationMessage.new(
-  message: "Less expectations, more satisfaction",
-)
-
-affirm_happy_four.user = user
-affirm_happy_four.mood = mood_happy
-affirm_happy_four.save!
-
-affirm_sad = AffirmationMessage.new(
-  message: "Remember to take a break if you're overwhelmed!",
-)
-
-affirm_sad.user = user_one
-affirm_sad.mood = mood_sad
-affirm_sad.save!
-
-affirm_sad_one = AffirmationMessage.new(
-  message: "You got this!",
-)
-
-affirm_sad_one.user = user_one
-affirm_sad_one.mood = mood_sad
-affirm_sad_one.save!
-
-affirm_sad_two = AffirmationMessage.new(
-  message: "I am so proud of you! Have a great day ahead",
-)
-
-affirm_sad_two.user = user_one
-affirm_sad_two.mood = mood_sad
-affirm_sad_two.save!
-
-affirm_sad_three = AffirmationMessage.new(
-  message: "You can do this! I am sure of it.",
-)
-
-affirm_sad_three.user = user_one
-affirm_sad_three.mood = mood_sad
-affirm_sad_three.save!
-
-affirm_sad_four = AffirmationMessage.new(
-  message: "You are not alone in this. Heads up!",
-)
-
-affirm_sad_four.user = user_one
-affirm_sad_four.mood = mood_sad
-affirm_sad_four.save!
-
-affirm_angry_one = AffirmationMessage.new(
-  message: "take a deep breath",
-)
-
-affirm_angry_one.user = user_one
-affirm_angry_one.mood = mood_angry
-affirm_angry_one.save!
-
-affirm_angry_two = AffirmationMessage.new(
-  message: "focus on yourself, not others",
-)
-
-affirm_angry_two.user = user_one
-affirm_angry_two.mood = mood_angry
-affirm_angry_two.save!
-
-affirm_scared_one = AffirmationMessage.new(
-  message: "don't be scared, you will be fine",
-)
-
-affirm_scared_one.user = user_one
-affirm_scared_one.mood = mood_scared
-affirm_scared_one.save!
-
-affirm_scared_two = AffirmationMessage.new(
-  message: "it is okay to feel scared, but i believe you will overcome it!",
-)
-
-affirm_scared_two.user = user_one
-affirm_scared_two.mood = mood_scared
-affirm_scared_two.save!
-
-affirm_love_one = AffirmationMessage.new(
-  message: "right people know your worth",
-)
-
-affirm_love_one.user = user_one
-affirm_love_one.mood = mood_love
-affirm_love_one.save!
-
-affirm_love_two = AffirmationMessage.new(
-  message: "putting yourself first is not selfish, it's your priority",
-)
-
-affirm_love_two.user = user_one
-affirm_love_two.mood = mood_love
-affirm_love_two.save!
-
-affirm_confused_one = AffirmationMessage.new(
-  message: "it is okay to not get have your life together all the time",
-)
-affirm_confused_one.user = user_one
-affirm_confused_one.mood = mood_confused
-affirm_confused_one.save!
-
-affirm_confused_two = AffirmationMessage.new(
-  message: "It is fine to get lost along the way, you will eventually reach your goal.",
-)
-affirm_confused_two.user = user_one
-affirm_confused_two.mood = mood_confused
-affirm_confused_two.save!
-
-puts "Affirmation messages created"
