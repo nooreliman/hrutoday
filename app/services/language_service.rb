e61370964a721cad2a33bda4ca3ffc1d08f09e7a
iman fuad <nooreliman@gmail.com>
2022-01-05 17:39:58 +0800

    commented the whole thing

--- app/services/language_service.rb
+++ app/services/language_service.rb
@@ -1,11 +1,11 @@
 # require "google/cloud/language/v1"
 
-class LanguageService
-  ::Google::Cloud::Language::V1::LanguageService::Client.configure do |config|
-    config.credentials = JSON.parse(ENV.fetch('GOOGLE_CREDS'))
-  end
-  @service ||= Google::Cloud::Language.language_service(version: :v1)
-  def self.analyze_sentiment(content)
-    @service.analyze_sentiment(document: {content: content, type: :PLAIN_TEXT})
-  end
-end
+# class LanguageService
+#   ::Google::Cloud::Language::V1::LanguageService::Client.configure do |config|
+#     config.credentials = JSON.parse(ENV.fetch('GOOGLE_CREDS'))
+#   end
+#   @service ||= Google::Cloud::Language.language_service(version: :v1)
+#   def self.analyze_sentiment(content)
+#     @service.analyze_sentiment(document: {content: content, type: :PLAIN_TEXT})
+#   end
+# end
